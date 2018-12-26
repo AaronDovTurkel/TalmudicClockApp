4245198170726e5e056138b0cc204f20bc2e2b9a
Aaron Dov Turkel <AaronDovTurkel@gmail.com>
2018-12-26 15:34:56 -0500

    finished basic functionality for talmudic clock.

--- index.js
+++ index.js
@@ -99,51 +99,70 @@ function fetch_results_store(results) {
 	});
 }
 
+let new_day = true;
+
+function new_day_toggle() {
+	return new Promise (function(resolve, reject) {
+		ip_geolocator_fetch()
+			.then(lat_lng => sun_rise_set_fetch(lat_lng))
+			.then(results => fetch_results_store(results)) // Do I need this???
+			.then(results => {
+				console.log(`this worked on 110`);
+				convertTimeToSecondsUtc(api_results.results.sunrise, clock.regularTime.timezoneOffset);
+				if (
+					info_store.timeInSecondsUtc >= clock.regularTime.regDayCurrentInSeconds
+				) {
+					console.log(`this 'if' worked on 115`);
+					new_day = false;
+				} else {
+					console.log(`this 'else' worked on 118`);
+					new_day = true;
+				}
+			})
+		resolve();
+	})
+};
+	
 //Come Together Function//
 // 1. Call 'D'
 // 2. .then Call 'E' (w/ response from 'D')
 // 3. function that creates variable that stores the results...
-function first_promise() {
+function before_sunrise_promise() {
 	ip_geolocator_fetch()
-		.then(lat_lng => sun_rise_set_fetch(lat_lng))
-		.then(results => fetch_results_store(results)) // Do I need this???
-		.then(results => {
-			console.log(`this worked on 111`);
-			if (
-				((((((parseInt((api_results.results.sunrise.split(":"))[0])) - clock.regularTime.timezoneOffset) * 60) + (parseInt((api_results.results.sunrise.split(":"))[1]))) * 60) + (parseInt((api_results.results.sunrise.split(":"))[2]))) >= clock.regularTime.regDayCurrentInSeconds
-			) {
-				console.log(`this 'if' worked on 115`);
-				ip_geolocator_fetch()
-					.then(lat_lng => sun_rise_set_fetch_alt(lat_lng))
-					.then(results => fetch_results_store(results))
-					.then(results => day_night_length_calculator(results))
-					.then(clock => {
-						time_sync(clock);
-						if (clock.day_clock.dayLengthInSeconds > ((86400 - (((((((parseInt((api_results.results.sunset.split(":"))[0])) - clock.regularTime.timezoneOffset) + 12) * 60) + (parseInt((api_results.results.sunset.split(":"))[1]))) * 60) + (parseInt((api_results.results.sunset.split(":"))[2])))) + (clock.regularTime.regDayCurrentInSeconds))) {
-							console.log(`this 'if' is being called on 123`);
-							day_ticker_trigger(clock);
-						} else {
-							console.log(`this 'else' is being called on 126`);
-							night_ticker_trigger(clock);
-						}
-					})
+		.then(lat_lng => sun_rise_set_fetch_alt(lat_lng))
+		.then(results => fetch_results_store(results))
+		.then(results => day_night_length_calculator(results))
+		.then(clock => {
+			convertTimeToSecondsUtc(api_results.results.sunset, clock.regularTime.timezoneOffset);
+			time_sync(clock);
+			if (clock.regularTime.regDayCurrentInSeconds < (86400 - info_store.timeInSecondsUtc)) {
+				console.log(`this 'if' is being called on 139`);
+				day_ticker_trigger(clock);
 			} else {
-				console.log(`this 'else' worked on 131`);
-				ip_geolocator_fetch()
-					.then(lat_lng => sun_rise_set_fetch(lat_lng))
-					.then(results => fetch_results_store(results))
-					.then(results => day_night_length_calculator(results))
-					.then(clock => {
-						time_sync(clock);
-						if (clock.day_clock.dayLengthInSeconds > ((86400 - (((((((parseInt((api_results.results.sunset.split(":"))[0])) - clock.regularTime.timezoneOffset) + 12) * 60) + (parseInt((api_results.results.sunset.split(":"))[1]))) * 60) + (parseInt((api_results.results.sunset.split(":"))[2])))) + (clock.regularTime.regDayCurrentInSeconds))) {
-							day_ticker_trigger(clock);
-						} else {
-							night_ticker_trigger(clock);
-						}
-					})
+				console.log(`this 'else' is being called on 142`);
+				night_ticker_trigger(clock);
 			}
 		})
-}
+};
+
+function after_sunrise_promise() {
+	ip_geolocator_fetch()
+		.then(lat_lng => sun_rise_set_fetch(lat_lng))
+		.then(results => fetch_results_store(results))
+		.then(results => day_night_length_calculator(results))
+		.then(clock => {
+			convertTimeToSecondsUtc(api_results.results.sunset, clock.regularTime.timezoneOffset);
+			time_sync(clock);
+			if (clock.regularTime.regDayCurrentInSeconds < (86400 - info_store.timeInSecondsUtc)) {
+				console.log(`this 'if' is being called on 157`);
+				day_ticker_trigger(clock);
+			} else {
+				console.log(`this 'else' is being called on 160`);
+				night_ticker_trigger(clock);
+			}
+		})
+};
+
 
 /*The Math Behind the Talmudic Clock Side (TMBTCS)*/
 
@@ -155,7 +174,6 @@ let clock =
 				hours: 00,
 				minutes: 00,
 				seconds: 00,
-				milliseconds: 0000,
 				talmudicDayMinute: 0,
 				dayLengthInSeconds: 0,
 				currentTalmudicSecondFromSunrise: 0
@@ -165,7 +183,6 @@ let clock =
 				hours: 00,
 				minutes: 00,
 				seconds: 00,
-				milliseconds: 0000,
 				talmudicNightMinute: 0,
 				nightLengthInSeconds: 0,
 				currentTalmudicSecondFromSunset: 0
@@ -188,34 +205,32 @@ let clock =
 //B. Calculate day length and night length in seconds from "The API Side- Basic".//
 function day_night_length_calculator() {
 	return new Promise (function(resolve, reject) { // is there a way to clean this all up?
-		let currentSunsetInSeconds = (((((((parseInt((api_results.results.sunset.split(":"))[0])) - clock.regularTime.timezoneOffset) + 12) * 60) + (parseInt((api_results.results.sunset.split(":"))[1]))) * 60) + (parseInt((api_results.results.sunset.split(":"))[2])));
-		clock.regularTime.sunsetToday = currentSunsetInSeconds;
-		let currentSunriseInSeconds = ((((((parseInt((api_results.results.sunrise.split(":"))[0])) - clock.regularTime.timezoneOffset) * 60) + (parseInt((api_results.results.sunrise.split(":"))[1]))) * 60) + (parseInt((api_results.results.sunrise.split(":"))[2])));
-		clock.regularTime.sunriseToday = currentSunriseInSeconds;
-		let dayLength = api_results.results.day_length;
-		let splitDayLength = dayLength.split(":");
-		clock.day_clock.dayLengthInSeconds = ((((parseInt(splitDayLength[0]) * 60) + (parseInt(splitDayLength[1]))) * 60) + (parseInt(splitDayLength[2])));
+		clock.regularTime.sunsetToday = convertTimeToSecondsUtc(api_results.results.sunset, clock.regularTime.timezoneOffset);
+		clock.regularTime.sunriseToday = convertTimeToSecondsUtc(api_results.results.sunrise, clock.regularTime.timezoneOffset);
+		clock.day_clock.dayLengthInSeconds = convertTimeToSeconds(api_results.results.day_length);
 		clock.night_clock.nightLengthInSeconds = ((86400) - (clock.day_clock.dayLengthInSeconds));
 		let twelvePartsDay = clock.day_clock.dayLengthInSeconds / 12;
 		let twelvePartsNight = clock.night_clock.nightLengthInSeconds / 12;
 		clock.day_clock.talmudicDayMinute = ((twelvePartsDay) / 60);
 		clock.night_clock.talmudicNightMinute = ((twelvePartsNight) / 60);
-		if (((clock.regularTime.regDayCurrentInSeconds) - (currentSunriseInSeconds)) >= 0) {
-			console.log(`this 'if' worked on 204`);
-			let currentTalmudicSecondFromSunset = ( (clock.day_clock.currentTalmudicSecondFromSunrise) - (clock.day_clock.dayLengthInSeconds) );
-			clock.night_clock.currentTalmudicSecondFromSunset = currentTalmudicSecondFromSunset;
+		if (((clock.regularTime.regDayCurrentInSeconds - clock.regularTime.sunriseToday) >= 0) && ((clock.regularTime.regDayCurrentInSeconds - clock.regularTime.sunriseToday) <= clock.day_clock.dayLengthInSeconds)) {
+			// After sunrise but before sunset
+			console.log(`this 'if' worked on 218`);
+			clock.day_clock.currentTalmudicSecondFromSunrise = (clock.regularTime.regDayCurrentInSeconds) - (clock.regularTime.sunriseToday);
+			clock.night_clock.currentTalmudicSecondFromSunset = 00;
+		} else if ((((clock.regularTime.regDayCurrentInSeconds) - (clock.regularTime.sunriseToday)) <= 0) && (((clock.regularTime.regDayCurrentInSeconds) - (clock.regularTime.sunriseToday)) <= clock.regularTime.sunriseToday)) {
+			// Before sunrise but after midnight
+			console.log(`this 'else if' worked on 223`);
+			clock.day_clock.currentTalmudicSecondFromSunrise = 00;
+			clock.night_clock.currentTalmudicSecondFromSunset = ((clock.night_clock.nightLengthInSeconds) + (clock.regularTime.regDayCurrentInSeconds));
+		} else if ((((clock.regularTime.regDayCurrentInSeconds - clock.regularTime.sunriseToday)) >= (clock.day_clock.dayLengthInSeconds)) && ((clock.regularTime.regDayCurrentInSeconds >= clock.day_clock.dayLengthInSeconds))) {
+			// after sunset but before midnight
+			console.log(`This 'else if' worked on 228`)
+			clock.day_clock.currentTalmudicSecondFromSunrise = 00;
+			clock.night_clock.currentTalmudicSecondFromSunset = (clock.regularTime.regDayCurrentInSeconds - (clock.regularTime.sunsetToday + 43200));
 		} else {
-			console.log(`this 'else' worked on 208`);
-			let currentTalmudicSecondFromSunset = ((86400 - currentSunsetInSeconds) + (clock.regularTime.regDayCurrentInSeconds));
-			clock.night_clock.currentTalmudicSecondFromSunset = currentTalmudicSecondFromSunset;
-		};
-		if ((clock.regularTime.regDayCurrentInSeconds) - (currentSunriseInSeconds) >= 0) {
-			let currentTalmudicSecondFromSunrise = (clock.regularTime.regDayCurrentInSeconds) - (currentSunriseInSeconds);
-			clock.day_clock.currentTalmudicSecondFromSunrise = currentTalmudicSecondFromSunrise;
-		} else {
-			let currentTalmudicSecondFromSunrise = ((86400 - currentSunriseInSeconds) + (clock.regularTime.regDayCurrentInSeconds));
-			clock.day_clock.currentTalmudicSecondFromSunrise = currentTalmudicSecondFromSunrise;
-		};
+			console.log(`Error: Something went wrong with day_night_length_calculator() on line 232`)
+		}
 		resolve(clock);
 	});
 }
@@ -235,11 +250,11 @@ function day_ticker_trigger(clock) {
 			console.log(dayClockArray);
 			day_minute_hour_ticker_checker(clock);
 			reg_time_pull();
-			dayClockArray = `This is the current Talmudic day time: ${clock.day_clock.hours}:${clock.day_clock.minutes}:${clock.day_clock.seconds}`;
+			dayClockArray = `${clock.day_clock.hours}:${clock.day_clock.minutes}:${clock.day_clock.seconds}`;
 			console.log(regClockArray);
 		}, 1000)
 		dayTicker;	
-		resolve(resultsTwo);	
+		resolve(clock);	
 	})
 }
 
@@ -249,7 +264,7 @@ function night_ticker_trigger(clock) {
 		console.log(nightClockArray);
 		night_minute_hour_ticker_checker(clock);
 		reg_time_pull();
-		nightClockArray = `This is the current Talmudic night time: ${clock.night_clock.hours}:${clock.night_clock.minutes}:${clock.night_clock.seconds}`;
+		nightClockArray = `${clock.night_clock.hours}:${clock.night_clock.minutes}:${clock.night_clock.seconds}`;
 		console.log(regClockArray);
 	}, 1000)
 	nightTicker;
@@ -310,8 +325,8 @@ function reg_time_pull() {
 	clock.regularTime.seconds = regularTimePull.getSeconds();
 	clock.regularTime.milliseconds = regularTimePull.getMilliseconds();
 	clock.regularTime.timezoneOffset = ((regularTimePull.getTimezoneOffset()) / 60);
-	clock.regularTime.regDayCurrentInSeconds = convertTimeToSeconds(clock.regularTime.hours, clock.regularTime.minutes, clock.regularTime.seconds);
-	regClockArray = `This is the current regular time: ${clock.regularTime.hours}:${clock.regularTime.minutes}:${clock.regularTime.seconds}`;
+	clock.regularTime.regDayCurrentInSeconds = convertTimeToSeconds(clock.regularTime);
+	regClockArray = `${clock.regularTime.hours}:${clock.regularTime.minutes}:${clock.regularTime.seconds}`;
 }
 
 let nightClockArray = '';
@@ -320,26 +335,22 @@ let dayClockArray = '';
 
 function time_sync(clock) {
 	return new Promise ((resolve, reject) => {	
-		if (clock.night_clock.currentTalmudicSecondFromSunset > clock.night_clock.nightLengthInSeconds) {
+		if (((clock.regularTime.regDayCurrentInSeconds - clock.regularTime.sunriseToday) >= 0) && ((clock.regularTime.regDayCurrentInSeconds - clock.regularTime.sunriseToday) <= clock.day_clock.dayLengthInSeconds)) {
+			// After sunrise but before sunset
 			let currentTalmudicDayTimeInMinutes = (clock.day_clock.currentTalmudicSecondFromSunrise / clock.day_clock.talmudicDayMinute);
-			let currentTalmudicDayTimeInMinutesSplit = currentTalmudicDayTimeInMinutes.toString().split('.');
-			let currentTalmudicDayHour = (currentTalmudicDayTimeInMinutesSplit[0] / 60);
-			let currentTalmudicDayHourSplit = currentTalmudicDayHour.toString().split('.');
-			clock.day_clock.hours = parseInt(currentTalmudicDayHourSplit[0]);
-			if (currentTalmudicDayTimeInMinutes <= 59) {
-				clock.day_clock.minutes = parseInt(currentTalmudicDayTimeInMinutesSplit[0]);
-			} else {
-				clock.day_clock.minutes = parseInt((String(((parseInt(currentTalmudicDayHourSplit[1].substring(0, 3))) * 600) / 1000)).substring(0, 2));
-			};
-			clock.day_clock.seconds = parseInt((String(((parseInt(currentTalmudicDayTimeInMinutesSplit[1].substring(0, 2))) * clock.day_clock.talmudicDayMinute) / 100)).substring(0, 2));
-			console.log(clock.day_clock);
+			let currentTalmudicDayHour = (currentTalmudicDayTimeInMinutes / 60);
+			clock.day_clock.hours = Math.trunc(currentTalmudicDayHour);
+			clock.day_clock.minutes = extractMinutesOrSecondsFromDecimal(currentTalmudicDayHour);
+			clock.day_clock.seconds = extractMinutesOrSecondsFromDecimal(currentTalmudicDayTimeInMinutes);
 		} else {
 			let currentTalmudicNightTime = (clock.night_clock.currentTalmudicSecondFromSunset);
 			let currentTalmudicNightTime_secondsPerHour = currentTalmudicNightTime / clock.night_clock.talmudicNightMinute;
-			let currentTalmudicNightTime_secondsPerHourSplit = currentTalmudicNightTime_secondsPerHour.toString().split('.');
-			let currentTalmudicNightHour = (currentTalmudicNightTime_secondsPerHourSplit[0] / 60);
-			let currentTalmudicNightHourSplit = currentTalmudicNightHour.toString().split('.');
-			clock.night_clock.hours = parseInt(currentTalmudicNightHourSplit[0]);
+			let test = currentTalmudicNightTime_secondsPerHour / 60;
+			console.log(test);
+			let test2 = (test) % 60;
+			console.log(test2);
+			clock.night_clock.hours = Math.trunc(test);
+			console.log(clock.night_clock.hours);
 			if (currentTalmudicNightTime_secondsPerHourSplit[0] <= 59) {
 				clock.night_clock.minutes = parseInt(currentTalmudicNightTime_secondsPerHourSplit[0]);
 			} else {
@@ -366,13 +377,112 @@ function time_sync(clock) {
 // 
 
 
-/*Run All Functions*/
-reg_time_pull();
-first_promise();
-
 //Simplifying Functions//
-function convertTimeToSeconds(hours, minutes, seconds) {
-	return ((((hours * 60) + (minutes)) * 60)  + seconds);
+function convertTimeToSeconds(time) {
+	if (typeof time === "string" || time instanceof String) {
+		splitAndParseInt(time);
+		info_store.timeInSeconds = ((((info_store.parsedArray[0] * 60) + (info_store.parsedArray[1])) * 60)  + info_store.parsedArray[2]);
+	} else if (typeof time === "object" || time instanceof Object) {
+		info_store.timeInSeconds = ((((time.hours * 60) + (time.minutes)) * 60)  + time.seconds);
+	} else {
+		info_store.timeInSeconds = ((((time[0] * 60) + (time[1])) * 60)  + time[2]);
+	};
+	return info_store.timeInSeconds;
+}
+
+function convertTimeToSecondsUtc(time, utc) {
+	if (typeof time === "string" || time instanceof String) {
+		splitAndParseInt(time);
+		info_store.timeInSecondsUtc = (((((info_store.parsedArray[0] - utc) * 60) + (info_store.parsedArray[1])) * 60)  + info_store.parsedArray[2]);
+	} else if (typeof time === "object" || time instanceof Object) {
+		info_store.timeInSecondsUtc = (((((time.hours - utc) * 60 ) + (time.minutes)) * 60)  + time.seconds);
+	} else {
+		info_store.timeInSecondsUtc = (((((time[0] - utc) * 60) + (time[1])) * 60)  + time[2]);
+	};
+	return info_store.timeInSecondsUtc;
+}
+
+function splitAndParseInt(time) {
+	info_store.parsedArray = [(parseInt((time.split(":"))[0])), (parseInt((time.split(":"))[1])), (parseInt((time.split(":"))[2]))];
+}
+
+function spliceDecimalPoint(int) {
+	let numberSplit = (String(int)).split(".");
+	console.log(`line 441. ` + numberSplit);
+	let splitParse = parseInt(numberSplit[1]);
+	info_store.minuteConversion = splitParse
+	return info_store.minuteConversion;
+}
+
+function getlength(int) {
+	console.log((`line 448. `) + ((((String(int)).replace('.', '')).length) - (String((Math.trunc(int)))).length));
+    return (((String(int)).replace('.', '')).length) - (String((Math.trunc(int)))).length;
+}
+
+function padZero1(int) {
+	let paddedNumber = `1`;
+	for (i = 0; i < int; i++) {
+		paddedNumber = paddedNumber + `0`;
+	}
+	return parseInt(paddedNumber);
+}
+
+function padZero(int) {
+	let paddedNumber = `number`;
+	for (i = 0; i < int; i++) {
+		paddedNumber = paddedNumber + `0`;
+	}
+	let parsedNumber = parseInt(paddedNumber)
+	return parsedNumber;
+}
+
+function extractMinutesOrSecondsFromDecimal(number) {
+	let finalResult = 00;
+	if ((number - (Math.floor(number))) !== 0) {
+		spliceDecimalPoint(number);
+		console.log(`line 472. ` + info_store.minuteConversion);
+		let lengthOfDecimal = getlength(number);
+		console.log(`line 474. ` + lengthOfDecimal);
+		let minuteConverted = ((info_store.minuteConversion * 60) / padZero1(lengthOfDecimal));
+		console.log(`line 476. ` + minuteConverted);
+		if ((String(minuteConverted)).includes("e") === true) {
+			finalResult = 00;
+			console.log(`line 479. ` + ((String(minuteConverted)).includes("e") === true));
+		} else {
+			finalResult = (parseInt(String(minuteConverted).substring(0, 2)));
+			console.log(`line 482. ` + (parseInt(String(minuteConverted).substring(0, 2))));
+		}
+		/*if ((String(finalResult)).length === 1) {
+			finalResult = `0${finalResult}`;
+		}*/
+		return finalResult;
+	} else {
+		return 00;
+	}
 }
 
 
+/*Extra Store*/
+
+let info_store = {
+	timeInSeconds: '',
+	timeInSecondsUtc: '',
+	parsedArray: '',
+	minuteConversion: 0
+}
+
+
+/*Run All Functions*/
+function run_all_functions() {
+	reg_time_pull();
+	new_day_toggle()
+		.then(() => {
+			if (new_day === true) {
+				after_sunrise_promise();
+			} else {
+				before_sunrise_promise();
+			}
+		})
+}
+
+run_all_functions();
